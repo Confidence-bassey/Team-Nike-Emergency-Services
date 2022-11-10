a78734c3e84c2428fa696537390e8666c034a583
Confidence-bassey <confidencebass2@gmail.com>
2022-11-10 20:56:00 +0100

    teamNikeServices first commit

---
+++ src/main/java/com/nike/teamNikeServices/TeamNikeServicesApplication.java
@@ -0,0 +1,13 @@
+package com.nike.teamNikeServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamNikeServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamNikeServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nike/teamNikeServices/TeamNikeServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nike.teamNikeServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamNikeServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
